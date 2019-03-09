97cd5990c50132d301fd6f467f6d8020aa4a88a5
urmzd <urmzds@gmail.com>
2019-03-09 14:29:27 -0400

    example

--- Attribute_Validator/EventIdValidator.py
+++ Attribute_Validator/EventIdValidator.py
@@ -1,14 +1,23 @@
 from bloom_filter import BloomFilter as bloom
+import csv
 
 
 class EventValidator:
 
     bloom = bloomFilter
+    last = -1;
+    current = -1;
 
     def __init__(self, values):
         self.values = values
 
-    def checkOrder(self, values):
-    
-    
-
+    def checkOrder(self, value):
+        
+        if (last == current and last == -1) :
+            last = value
+        else:
+            current = value;
+            if (last > current) :
+                print("Error")
+            else :
+                last = current
